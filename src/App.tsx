255fafa2deb201abad8a989eb8179813d9693b56
Giuran Radu <giuran.radu@yahoo.com>
2026-02-11 00:21:50 +0200

    this commit should be rejected

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 export default function App() {
+  const a = 10
   return (
   <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white text-4xl">Tailwind funcționează</div>
   )
